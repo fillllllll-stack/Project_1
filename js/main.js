ba53ba03eaece246645939781128c656f197285e
fillllllll-stack <filip.gorea@ati.utm.md>
2020-06-04 19:41:20 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+/*
+|‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒
+| Carousel
+|‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒‒
+*/
+
+
+
+$('.dropdown-toggle').click(function(e) {
+    if ($(document).width() > 320) {
+        e.preventDefault();
+        var url = $(this).attr('href');
+        if (url !== '#') {
+            window.location.href = url;
+        }
+    }
+});
